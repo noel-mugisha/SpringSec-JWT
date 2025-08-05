a2122ad948c3421cc4325a9479dfc7dda138973a
Noel <noelmugisha332@gmail.com>
2025-08-05 13:54:39 +0200

    Added User model, service, UserPrincipal and CustomUserDetailsService classes

---
+++ src/main/java/com/demo/springjwt/enums/Role.java
@@ -0,0 +1,6 @@
+package com.demo.springjwt.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/demo/springjwt/model/User.java
@@ -0,0 +1,26 @@
+package com.demo.springjwt.model;
+
+import com.demo.springjwt.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
